423ddcb616302926a6600693418dea031523a2de
whitetiger <whitetigerv@naver.com>
2025-05-01 12:36:15 +0900

    /** 2025.05.01 &&/ 1. 효진이 작업 대상 패키지 경로 변경, TODO 수정

---
+++ src/main/java/com/movie/movie/dailyBoxOffice/MovieController.java
@@ -0,0 +1,14 @@
+package com.movie.movie.dailyBoxOffice;
+
+public class MovieController {
+
+	// TODO - 1. 일별 박스오피스 조회 API
+
+	// TODO - 2. 주간/주말 박스 오피스 조회 API - 패키지별로 분리 필요
+
+	// TODO - 3. 영화 상세정보 조회 API  - 패키지별로 분리 필요
+
+	// TODO - 4. 영화사 정보, 영화사 상세정보 조회 API  - 패키지별로 분리 필요
+
+	// TODO - 5. 영화인 정보, 영화인 상세정보 조회 API  - 패키지별로 분리 필요
+}
